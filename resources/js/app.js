c094d832418a0c3150e88a1263b9fcf263def52d
natsume0718 <moto718program@gmail.com>
2021-10-11 22:50:46 +0900

    remove vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,10 @@ require('./bootstrap');
 import Vue from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
 import PortalVue from 'portal-vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
 Vue.use(PortalVue);
-Vue.use(Vuetify);
 
 const app = document.getElementById('app');
 
@@ -21,6 +18,5 @@ new Vue({
                 initialPage: JSON.parse(app.dataset.page),
                 resolveComponent: (name) => require(`./Pages/${name}`).default,
             },
-        }),
-    vuetify: new Vuetify()
+        })
 }).$mount(app);
