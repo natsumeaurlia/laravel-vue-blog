fe5e09e21b2dc97388611d923778b77ed8b0f408
natsume0718 <moto718program@gmail.com>
2021-10-17 21:49:13 +0900

    fix UpdateTeamNameTest

--- tests/Feature/UpdateTeamNameTest.php
+++ tests/Feature/UpdateTeamNameTest.php
@@ -14,7 +14,7 @@ class UpdateTeamNameTest extends TestCase
     {
         $this->actingAs($user = User::factory()->withPersonalTeam()->create());
 
-        $response = $this->put('/teams/'.$user->currentTeam->id, [
+        $response = $this->put(route('teams.update', [$user->currentTeam->id]), [
             'name' => 'Test Team',
         ]);
 
