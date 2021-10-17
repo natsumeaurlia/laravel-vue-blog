bd4c860eda00f02b4fda5cb16a564f77f6ced942
natsume0718 <moto718program@gmail.com>
2021-10-17 18:20:56 +0900

    add card

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="border rounded bg-white border-gray-100">
+        <div class="p-6 flex flex-col">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+</script>
+
