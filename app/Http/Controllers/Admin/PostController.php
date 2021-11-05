90c24b2c30e11780ebe88451971f2c11f91e9d19
natsume0718 <moto718program@gmail.com>
2021-11-06 01:55:25 +0900

    store post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class PostController extends Controller
@@ -15,5 +16,20 @@ class PostController extends Controller
     {
         $posts = Post::with(['user'])->paginate(self::PAGINATE);
         return Inertia::render('Admin/Posts/Index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Admin/Posts/Create');
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        $post = new Post();
+        $data = array_merge($request->validated(), ['user_id' => Auth::id()]);
+        if (!$post->fill($data)->save()) {
+            return redirect()->route('admin.post.index')->with('failed-message', 'Create Failed');
+        }
+        return redirect()->route('admin.post.index')->with('success-message', 'Create Successful');
     }
 }
